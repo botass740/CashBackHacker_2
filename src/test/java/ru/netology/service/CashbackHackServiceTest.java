a6643cad3288ef5d98187a788cfa7d4099c420e3
Max test <botass740@gmail.com>
2023-07-09 22:45:46 +0700

    Create branch TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,42 +1,42 @@
 package ru.netology.service;
-
-import static org.junit.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRomain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRomainZero() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRomainBorderUnder() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRomainBorderAbove() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int actual = service.remain(amount);
         int expected = 999;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
+
 }
